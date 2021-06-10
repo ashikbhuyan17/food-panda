93f2c5fc96fed1c4e614146665cb17cee48bfff0
ashikbhuyan17 <ashikbhuyan.swe.diu@gmail.com>
2021-06-10 20:35:24 +0600

    food-panda

---
+++ src/App.js
@@ -0,0 +1,17 @@
+
+import './App.css';
+import Content from './components/Content';
+import Hero from './components/Hero';
+import Navbar from './components/Navbar';
+
+function App() {
+  return (
+    <>
+        <Navbar/>
+        <Hero/>
+        <Content/>
+    </>
+  );
+}
+
+export default App;
